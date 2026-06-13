1b3ab99d528eeeb71bbc121bdadb7f1936a12dac
deadw4lker <iamsagarraj54@gmail.com>
2026-06-14 00:38:25 +0530

    Fix OPTIONS preflight crash: send ASGI response directly instead of using Response class

--- middleware.py
+++ middleware.py
@@ -1,5 +1,13 @@
 from starlette.types import ASGIApp, Scope, Receive, Send
-from starlette.responses import Response
+
+
+CORS_HEADERS = [
+    (b"access-control-allow-origin", b"https://vinkspace.fun"),
+    (b"access-control-allow-methods", b"GET, POST, PUT, DELETE, PATCH, OPTIONS"),
+    (b"access-control-allow-headers", b"authorization, content-type, x-requested-with, accept"),
+    (b"access-control-max-age", b"600"),
+    (b"access-control-allow-credentials", b"false"),
+]
 
 
 class ForceCORSMiddleware:
@@ -9,24 +17,18 @@ class ForceCORSMiddleware:
     async def __call__(self, scope: Scope, receive: Receive, send: Send):
         if scope["type"] == "http":
             if scope["method"] == "OPTIONS":
-                headers = [
-                    (b"access-control-allow-origin", b"https://vinkspace.fun"),
-                    (b"access-control-allow-methods", b"GET, POST, PUT, DELETE, PATCH, OPTIONS"),
-                    (b"access-control-allow-headers", b"authorization, content-type, x-requested-with, accept"),
-                    (b"access-control-max-age", b"600"),
-                    (b"access-control-allow-credentials", b"false"),
-                ]
-                response = Response(status_code=200, headers=dict(headers))
-                await response(scope, receive, send)
+                await send({
+                    "type": "http.response.start",
+                    "status": 200,
+                    "headers": CORS_HEADERS,
+                })
+                await send({"type": "http.response.body", "body": b""})
                 return
 
             async def send_with_cors(message):
                 if message["type"] == "http.response.start":
                     headers = list(message.get("headers", []))
-                    headers.append((b"access-control-allow-origin", b"https://vinkspace.fun"))
-                    headers.append((b"access-control-allow-methods", b"GET, POST, PUT, DELETE, PATCH, OPTIONS"))
-                    headers.append((b"access-control-allow-headers", b"authorization, content-type, x-requested-with, accept"))
-                    headers.append((b"access-control-allow-credentials", b"false"))
+                    headers.extend(CORS_HEADERS)
                     message["headers"] = headers
                 await send(message)
 
